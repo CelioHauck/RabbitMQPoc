cb23e8880d5fb91809b35a2e16ee255e1bf4ea6d
Celio Hauck <chjunior1@stefanini.com>
2019-11-27 14:47:36 -0300

    first commit

---
+++ Consumer/Consumir.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Consumir
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Celio",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(message);
+                    };
+
+                    channel.BasicConsume(queue: "Celio",
+                                          autoAck: true,
+                                          consumer: consumer);
+                    Console.WriteLine("Consumer Funcionando");
+                    Console.ReadLine();
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Send/Enviar.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQTeste
+{
+
+
+    class Enviar
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue:"Celio",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = "Hello World";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "Celio",
+                                         basicProperties: null,
+                                         body:body
+                                         );
+                    Console.WriteLine("Mensagem enviada!");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
